e3b141f8787298ef2787d273b6d63cd6991196e3
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-02 10:40:47 +0900

    remove unnecessary button

--- lib/main.dart
+++ lib/main.dart
@@ -74,11 +74,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
