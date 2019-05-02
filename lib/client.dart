a2906b3076776918e21f3f32e984b87c12d8a49c
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-02 10:10:59 +0900

    build url

--- lib/client.dart
+++ lib/client.dart
@@ -2,17 +2,18 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:nyt_best_sellers/creds.dart';
 import 'package:nyt_best_sellers/list_names_response.dart';
 import 'package:nyt_best_sellers/lists_response.dart';
 
 class Client {
-  static const _BASE_URL = "https://api.nytimes.com/";
+  static const _HOST = "api.nytimes.com";
   static const _BASE_PATH = "svc/books/v3";
   static const _GET_LISTS_PATH = "$_BASE_PATH/lists.json";
   static const _GET_LIST_NAMES_PATH = "$_BASE_PATH/lists/names.json";
 
   Future<ListNamesResponse> getListNames() async {
-    final response = await http.get("$_BASE_URL$_GET_LIST_NAMES_PATH");
+    final response = await http.get(_buildUrl(_GET_LIST_NAMES_PATH, null));
     if (response.statusCode == 200) {
       return ListNamesResponse.fromJson(json.decode(response.body));
     } else {
@@ -21,10 +22,18 @@ class Client {
   }
 
   Future<ListsResponse> getLists() async {
-    final response = await http.get("$_BASE_URL$_GET_LISTS_PATH");
+    final response = await http.get(_buildUrl(_GET_LISTS_PATH, null));
     if (response.statusCode == 200) {
       return ListsResponse.fromJson(json.decode(response.body));
     }
     return ListsResponse.ERROR;
+  }
+
+  Uri _buildUrl(String path, Map<String, String> params) {
+    var queryParam = {"api-key": API_TOKEN};
+    if (params != null) {
+      queryParam.addAll(params);
+    }
+    return Uri.https(_HOST, path, queryParam);
   }
 }
